ae466b43dd2888f3ee53201defcd367bef2307d5
christianokada <christian.okada@gmail.com>
2017-07-27 10:50:02 -0400

    logs and comments

--- js/search.js
+++ js/search.js
@@ -4,12 +4,14 @@ function filter_results() {
     var div = document.getElementById("fh5co-main");
     var list = div.getElementsByTagName('a');
 
+    // get list of frame filenames in images.json
     $.getJSON('images.json', function(data) {
         for (i = 0; i < list.length; i++) {
             var contains;
+            // get frame[i] and compare tags with input query
             $.getJSON('data/' + parse_json(data[i], ".json"), function(json_data) {
                 element = parse_json(json_data.frame, "");
-                console.log("init = " + i, element)
+                // console.log("init = " + i, element)
                 contains = false;
                 tags = json_data.description.tags;
                 for (j = 0; j < tags.length; j++) {
@@ -19,14 +21,15 @@ function filter_results() {
                     }
                 }
             }).then(function() {
-                console.log("post = " + element + i)
+                // console.log("post = " + element + i)
+                // make hidden or visible if bool is false
                 if (contains == false) {
-                    console.log(element + "hidden")
+                    console.log(element + " hidden")
                     document.getElementById(element).setAttribute('style', 'display:none');
                     // document.getElementById(element).setAttribute('style','visibility:hidden');
                 }
                 else {
-                    console.log(element + "visibile")
+                    console.log(element + " visibile")
                     document.getElementById(element).setAttribute('style', 'display:block');
                     // document.getElementById(element).setAttribute('style','visibility:visible');
                 }
